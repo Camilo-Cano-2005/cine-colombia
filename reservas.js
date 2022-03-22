f884452b1386826c0763a5d3e05759dd376fe17a
Camilo-Cano-2005 <camicano2005@gmail.com>
2022-03-22 17:15:09 -0500

    terrf

--- reservas.js
+++ reservas.js
@@ -1,7 +1,9 @@
 let contenedor = document.getElementById("contenedor");
+let bandera = true
 
 contenedor.addEventListener("click", function (evento) {
     if (evento.target.classList.contains("w-100")) {
-        evento.target.src="img/rojo.png"
+        evento.target.src="img/rojo.png";
+        bandera = false;
     }
 })
